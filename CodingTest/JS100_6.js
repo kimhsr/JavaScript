ed8816a19c15fa251468a0a12dfa1d4ffbd6d794
kimhsr <kimhsr@naver.com>
2022-03-15 22:03:25 +0900

    False

---
+++ CodingTest/JS100_6.js
@@ -0,0 +1,11 @@
+var boolean1 = Boolean(NaN);
+var boolean2 = Boolean(1);
+var boolean3 = Boolean("");
+var boolean4 = Boolean(0);
+var boolean5 = Boolean(undefined);
+
+console.log(boolean1);
+console.log(boolean2);
+console.log(boolean3);
+console.log(boolean4);
+console.log(boolean5);
