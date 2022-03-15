d681faacaa6393dea04af4de04c6a21d6d4686c1
kimhsr <kimhsr@naver.com>
2022-03-16 03:20:24 +0900

    행성 문제2

---
+++ CodingTest/JS100_26.js
@@ -0,0 +1,14 @@
+const godtjd = {
+    '수성' : 'Mercury',
+    '금성' : 'Venus',
+    '지구' : 'Earth',
+    '화성' : 'Mars',
+    '목성' : 'Jupiter',
+    '토성' : 'Saturn',
+    '천왕성' : 'Uranus',
+    '해왕성' : 'Neptune',
+};
+
+const n = prompt("행성 이름을 적으세요");
+
+console.log(godtjd[n]);
