f3750aba0d70ba3d58b877721963a58c19e911dd
kimhsr <kimhsr@naver.com>
2022-03-15 21:52:19 +0900

    변수의 타입2

---
+++ CodingTest/JS100_4.js
@@ -0,0 +1,11 @@
+var arr = 1;
+console.log(typeof(arr));
+
+var arr = 2.22;
+console.log(typeof(arr));
+
+var arr = 'p';
+console.log(typeof(arr));
+
+var arr = [1, 2, 3];
+console.log(typeof(arr));
